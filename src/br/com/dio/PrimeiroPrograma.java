644a685998322810aba6b85e7cb354a3db542418
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2022-02-15 15:19:50 -0300

    First commit.

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,9 +6,45 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         Gato gato = new Gato();
+        Livro livro = new Livro("Harry Potter and the Deathly Hallows",1000);
+
 
         System.out.println(gato);
-        System.out.println("Poooontoooo!");
+        System.out.println(livro);
 
+    }
+}
+
+class Livro{
+    private String nome;
+    private int numPaginas;
+
+    public Livro(String nome, int numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(int numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
